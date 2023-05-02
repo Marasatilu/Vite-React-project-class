aafa404686e6d6f37fc0f0986d7a8faf73b6cc74
ggonzaloo10 <92878187+GitSepape@users.noreply.github.com>
2023-05-02 19:44:20 +0200

    add components

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,48 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import MyTopBarKickass from './componets/topbar/Topbar'
+
+
+import Topbar from './componets/topbar/Topbar'
 import './App.css'
 import './SCSS/global.scss'
-import Styleguide from './views/Styleguide'
+import Styleguide from './views/styleguide/Styleguide'
 import { Link, Route, Routes } from 'react-router-dom'
+import Contact from './views/Contact'
+
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <MyTopBarKickass />
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Link to="/styleguide"></Link>
 
-      <Routes>
+      
+      <header className='section'>
+        <h1> Project Gamma</h1>
+          <Link to="/contact">Contacto</Link>
+          <Link to="/styleguide">StyleGuide</Link>
+          <Link to="/">Home</Link>
+      </header>
+      <main>
+      <Topbar/>
+      <Contact/>
+          <Routes>
 
-        <Route path="/styleguide" element={ <Styleguide/>} />
-        
+            <Route path="/styleguide" element={ <Styleguide/>} />
+            
 
-      </Routes>
+          </Routes>
+      </main>
 
 
     </>
---
+++ vite-project/src/views/styleguide/Styleguide.jsx
@@ -0,0 +1,12 @@
+function Styleguide() {
+ 
+
+    return (
+      <>
+          <h1 className='styleguide'>Styleguide</h1>
+      </>
+    )
+  }
+  
+  export default Styleguide
+  
